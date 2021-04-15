3a13dfbc4dc65ff32ebe1199a620ff6007face38
Santeri Elo <me@santeri.xyz>
2021-04-15 20:04:17 +0300

    fix: sitemap env variable

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://akm.iffa.dev',
+  siteUrl: process.env.VERCEL_URL || 'https://akm.iffa.dev',
   generateRobotsTxt: true,
   exclude: ['*/404'],
 };
